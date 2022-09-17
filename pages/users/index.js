d10986ed5f7b09d18af601b83f412fb6d4f38de4
sdshuvo98 <hellosdshuvo@gmail.com>
2022-09-17 13:03:51 +0600

    manage dynamic routes

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,9 +1,22 @@
+import Link from 'next/link';
 import React from 'react'
 
 function index({ users }) {
     return (
         <div>
             <h1>This is users: {users.length}</h1>
+            {
+                users.map(user => (
+                    <div key={user.id}>
+                        <h3>
+                            Name: {user.name}
+                            <Link href={`/users/${user.id}`}>
+                                <button>Details</button>
+                            </Link>
+                        </h3>
+                    </div>
+                ))
+            }
         </div>
     )
 }
